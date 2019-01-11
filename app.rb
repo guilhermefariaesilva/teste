951f30daa741eb4e2fdd424983323fac00a07ee7
Gullit Miranda <gullitmiranda@gmail.com>
2019-01-11 19:38:36 -0200

    [redis] Adding support to env REDIS_PASSWORD

--- app.rb
+++ app.rb
@@ -5,14 +5,28 @@ require 'sinatra/reloader'
 require 'redis'
 require 'bundler/setup'
 
-set :redis, Redis.new(url: ENV['REDIS_URL'])
+redis_opts = {url: ENV['REDIS_URL']}
+
+if (password = ENV['REDIS_PASSWORD'])
+  redis_opts[:password] = password
+end
+
+set :redis, Redis.new(redis_opts)
 
 get '/' do
   'OK'
 end
 
+get '/ping' do
+  settings.redis.ping.to_s
+end
+
 get '/inc' do
   settings.redis.incr('counter').to_s
+end
+
+get '/counter' do
+  settings.redis.get('counter').to_s
 end
 
 get '/version' do
